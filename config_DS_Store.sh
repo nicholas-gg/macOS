e285cbe279ecd7c9ba2c113347db43f0b375cbe5
Nick <nick@MBP.local>
2024-06-03 03:58:30 +0800

    Small updates

--- config_DS_Store.sh
+++ config_DS_Store.sh
@@ -1,30 +1,28 @@
 #!/usr/bin/env sh
 
-_STATUS()
-{
-printf '%s\n' "Writing .DS_Store on $1 drives is currently $2."
-return
+_STATUS() {
+  printf '%s\n' "Writing .DS_Store on $1 drives is currently $2."
 }
 
 printf '%s' 'Reading configuration... '
 
 _NWSTATUS=$(defaults read com.apple.desktopservices DSDontWriteNetworkStores 2>/dev/null)
-if [[ $_NWSTATUS == true ]]; then
+if [ "$_NWSTATUS" = "true" ]; then
 	_NWSTATUS=disabled
-elif [[ $_NWSTATUS == false ]]; then
+elif [ "$_NWSTATUS" = "false" ]; then
 	_NWSTATUS=enabled
-elif [[ $_NWSTATUS == "" ]]; then
+elif [ -z "$_NWSTATUS" ]; then
 	_NWSTATUS='not configured (enabled)'
 else
 	_NWSTATUS=misconfigured
 fi
 
 _USBSTATUS=$(defaults read com.apple.desktopservices DSDontWriteUSBStores 2>/dev/null)
-if [[ $_USBSTATUS == 0 ]]; then
+if [ "$_USBSTATUS" = "0" ]; then
 	_USBSTATUS=enabled
-elif [[ $_USBSTATUS == 1 ]]; then
+elif [ "$_USBSTATUS" = "1" ]; then
 	_USBSTATUS=disabled
-elif [[ $_USBSTATUS == "" ]]; then
+elif [ -z "$_USBSTATUS" ]; then
 	_USBSTATUS='not configured (enabled)'
 else
 	_USBSTATUS=misconfigured
@@ -34,3 +32,4 @@ printf '%s\n' 'done.' ''
 
 _STATUS network "$_NWSTATUS"
 _STATUS removable "$_USBSTATUS"
+
